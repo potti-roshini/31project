b4baf566713c8c435040ce84afa139f3ecbff338
potti-roshini <91303288+potti-roshini@users.noreply.github.com>
2021-12-02 10:42:14 +0530

    Add files via upload

---
+++ EgSum1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CP1-41
+ */
+public class EgSum1 {
+    public static void main (String args[])
+    {
+        int a=2,b=2;
+        int sum=a+b;
+        System.out.println(sum);        
+    }             
+}
+    
+
